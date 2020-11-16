7f3577d5dfaae12a4ca73e5a1be223b3e3aa6c99
adilb <Adilbek-Akhmet>
2020-11-16 11:13:54 +0600

    Go project

---
+++ EmployeeInfo.go
@@ -0,0 +1,11 @@
+package main
+
+type Employee struct {
+	Name, Position string
+}
+
+func NewEmployeeFactory(position string) func(name string) *Employee {
+	return func(name string) *Employee {
+		return &Employee{name, position}
+	}
+}
